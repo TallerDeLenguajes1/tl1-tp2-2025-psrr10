e1b0c42e761e2eb1b3bae777d08e8f190de08f33
Paulo Srur <paulosrur777@gmail.com>
2025-04-11 12:09:32 -0300

    funcion valores aleatorios agregada

--- tp2_4.c
+++ tp2_4.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 struct compu
 {
     int velocidad;
     int anio;
     int cantidadNucleos;
-    char *tipoCpu; 
-}typedef Compu;
+    char *tipoCpu;
+} typedef Compu;
+
+char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+void generarPCs(Compu pcs[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        pcs[i].velocidad = rand() % 3 + 1;
+        pcs[i].anio = rand() % 10 + 2015;
+        pcs[i].cantidadNucleos = rand() % 8 + 1;
+        pcs[i].tipoCpu = tipos[rand() % 6];
+    }
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+
+    return 0;
+}
